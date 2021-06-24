4b3f120189cbe97599b266dfe04184d565c8a115
Jean Carlo <jeancarlomo@hotmail.com>
2021-06-24 07:58:14 -0300

    sign-up feito e sign-in falta acrescentar o token

--- src/app.js
+++ src/app.js
@@ -1,10 +1,103 @@
 import express from 'express'
 import cors from 'cors'
 import dayjs from 'dayjs'
+import pg from 'pg'
+import bcrypt from 'bcrypt';
 
 const app=express()
 app.use(cors())
 app.use(express.json())
+
+//database
+const{Pool} = pg
+
+const connection = new Pool({
+    user: 'postgres',
+    password: '123456',
+    host: 'localhost',
+    port: 5432,
+    database: 'mywallet'
+
+})
+
+app.get("/teste" , async (req,res)=>{
+
+   const result =  await connection.query(`
+    SELECT * FROM users
+    `)
+   res.send(result)
+})
+
+
+
+//Sign-up
+app.post("/sign-up" ,async(req,res)=>{
+
+    const {name,email,password} = req.body
+
+    console.log(password)
+
+    const passwordHash = bcrypt.hashSync(password,12)
+    
+    console.log(passwordHash)
+    
+    try{
+
+       const emalAlreadyRegistered = await connection.query(`
+            SELECT email FROM users
+            WHERE email = $1
+        `,[email])
+
+        console.log(emalAlreadyRegistered)
+
+        if(emalAlreadyRegistered.rows.length){
+            res.sendStatus(409)
+            return 
+        }
+        
+        await connection.query(`
+        INSERT INTO users (name,email,password) 
+        VALUES ($1,$2,$3)
+        `,[name,email,passwordHash])
+        res.sendStatus(200)
+    }catch(e){
+        console.log('Erro ao salvar o novo usuario')
+        console.log(e)
+        res.sendStatus(400)
+    }
+
+})
+
+//sign-in
+
+app.post("/sign-in" , async(req,res)=>{
+    const {email,password} = req.body
+
+    try{
+        const searchUser =await connection.query(`
+        SELECT email,password FROM users
+        WHERE email = $1 
+        `,[email]) 
+
+        console.log(searchUser.rows)
+        //return
+        const user = searchUser.rows[0]
+        
+        if(user && bcrypt.compareSync(password,user.password)){
+            console.log('logado')
+            res.sendStatus(200)
+        }else{
+            res.status(400).send('email ou senha incorretos')
+        }
+    }catch(e){
+        console.log('Erro na autenticação')
+        console.log(e)
+        res.sendStatus(400)
+    }
+        
+
+})
+
 
 app.get("/home", async(req,res)=>{
     const today = dayjs().format("YYYY/MM/DD")
