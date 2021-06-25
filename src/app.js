8b43f330afb1b4b6e9935fed09057f0cb07ae673
Jean Carlo <jeancarlomo@hotmail.com>
2021-06-25 10:35:07 -0300

    delete da seesao atual e remocao de comentarios

--- src/app.js
+++ src/app.js
@@ -1,25 +1,18 @@
 import express, { query } from 'express'
 import cors from 'cors'
 import dayjs from 'dayjs'
-import pg from 'pg'
+
 import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
+import connection from './database.js'
+
+
 
 const app=express()
 app.use(cors())
 app.use(express.json())
 
-//database
-const{Pool} = pg
 
-const connection = new Pool({
-    user: 'postgres',
-    password: '123456',
-    host: 'localhost',
-    port: 5432,
-    database: 'mywallet'
-
-})
 
 app.get("/teste" , async (req,res)=>{
 
@@ -36,11 +29,11 @@ app.post("/sign-up" ,async(req,res)=>{
 
     const {name,email,password} = req.body
 
-    console.log(password)
+    
 
     const passwordHash = bcrypt.hashSync(password,12)
     
-    console.log(passwordHash)
+   
     
     try{
 
@@ -49,7 +42,7 @@ app.post("/sign-up" ,async(req,res)=>{
             WHERE email = $1
         `,[email])
 
-        console.log(emalAlreadyRegistered)
+        
 
         if(emalAlreadyRegistered.rows.length){
             res.sendStatus(409)
@@ -72,7 +65,7 @@ app.post("/sign-up" ,async(req,res)=>{
 //sign-in
 
 app.post("/sign-in" , async(req,res)=>{
-   // console.log(req.body)
+   
     
     
     const {email,password} = req.body
@@ -83,12 +76,12 @@ app.post("/sign-in" , async(req,res)=>{
         WHERE email = $1 
         `,[email]) 
 
-       //console.log(searchUser.rows)
+       
         
         const user = searchUser.rows[0]
         
         if(user && bcrypt.compareSync(password,user.password)){
-            //console.log('verificado')
+            
             const token = uuid();
 
             await connection.query(`
@@ -103,7 +96,7 @@ app.post("/sign-in" , async(req,res)=>{
             }
             res.send(userData)
         }else{
-            res.status(400).send('email e/ou senha incorretos')
+            res.status(401).send('email e/ou senha incorretos')
         }
     }catch(e){
         console.log('Erro na autenticação')
@@ -116,22 +109,22 @@ app.post("/sign-in" , async(req,res)=>{
 
 
 app.get("/home", async(req,res)=>{
-    //const today = dayjs().format("YYYY/MM/DD")
-   console.log(dayjs().format("YYYY-MM-DD"))
-
-
-   console.log(req.headers['authorization'])
-
+    
    const token = req.headers["authorization"].replace("Bearer ",'')
-   console.log(token)
-
    try{
         const result = await connection.query(`
         SELECT "userId" FROM sessions WHERE token = $1
         `,[token])
 
+        if(result.rows.length===0){
+            res.status(401).send('Essa sessão expirou')
+            return
+        }
+
+        
+
         const userId = result.rows[0].userId
-       // console.log(userId)
+       
 
        const searchTransactions = await connection.query(`
        SELECT value,description,type,date
@@ -139,7 +132,7 @@ app.get("/home", async(req,res)=>{
        WHERE "userId" = $1
        `,[userId])
 
-       //console.log(searchTransactions.rows)
+       
        res.status(200).send(searchTransactions.rows)
 
 
@@ -149,49 +142,19 @@ app.get("/home", async(req,res)=>{
    }
 
 
-   
-  // return
-//     const obj =[
-//         {
-//         date:today,
-//         description:'Katon',
-//         value:3900,
-//         type:'deposit'
-//     },
-
-//     {
-//         date:today,
-//         description:'Suiton',
-//         value:1700,
-//         type:'withdraw'
-//     },
-
-//     {
-//         date:today,
-//         description:'Suiton',
-//         value:1459,
-//         type:'withdraw'
-//     }
-
-// ]
-
-//     res.send(obj)
+  
 })
 
 app.post("/entry" ,async(req,res)=>{
-    console.log(req.body)
-    console.log(req.headers['authorization'])
-    const{value,description,transactionType} = req.body
-
-    console.log(value)
-    const newValue=parseInt(value*100)
-    console.log(newValue)
-
-    const token =req.headers['authorization'].replace('Bearer ','')
-    const today = dayjs().format("YYYY/MM/DD")
-    console.log(token)
    
+    const{value,description,transactionType} = req.body
     
+    const newValue=parseInt(value*100)
+    
+    const token =req.headers['authorization'].replace('Bearer ','')
+    
+    const today = dayjs().format("YYYY/MM/DD")
+     
     try{
         const result = await connection.query(`
         SELECT * FROM sessions
@@ -208,18 +171,40 @@ app.post("/entry" ,async(req,res)=>{
         ($1,$2,$3,$4,$5)
         `,[newValue,description,transactionType,today,userId])
         
-        //console.log('minha primeira requsição fullstack')
+        
         res.sendStatus(200)
     }catch(e){
         console.log('erro ao salvar operação')
         console.log(e)
         res.sendStatus(400)
-        
     }
     
 })
 
-app.listen(4000, ()=>{
 
-    console.log('server dodando na 4000')
+app.post("/logout" , async(req,res)=>{
+    const {token} = req.body
+    
+    try{
+        await connection.query(`
+            DELETE FROM sessions
+            WHERE token = $1
+        `,[token])
+
+        res.sendStatus(200)
+    }catch(e){
+        console.log('Erro ao deletar sessão')
+        console.log(e)
+    }
+
+
 })
+
+
+app.get("/banana",(req,res)=>{
+    res.sendStatus(200)
+})
+
+
+
+export default app;
---
+++ tests/testes.test.js
@@ -0,0 +1,10 @@
+import app from '../src/app.js'
+import supertest from 'supertest'
+
+describe("GET /banana", () => {
+    it("returns status 200 for valid params", async() => {
+    const result = await supertest(app).get("/banana")        
+
+    expect(result.status).toEqual(200);
+    });
+}); 
