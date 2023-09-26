aaf4cc82861ef0f73217c5d22cd0d28c7c7112ba
Jean Carlo <jeancarlodev@gmail.com>
2023-09-26 16:58:20 -0300

    update dump.sql

--- dump.sql
+++ dump.sql
@@ -155,41 +155,20 @@ INSERT INTO public.sessions VALUES (22, 24, '89616654-315d-4b83-9b1e-73ad72a4801
 -- Data for Name: transactions; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.transactions VALUES (2, 14540, 'segundo depósito', 'deposit', '2021/06/24', 1);
-INSERT INTO public.transactions VALUES (3, 4540, 'primeira retirada', 'withdraw', '2021/06/24', 1);
-INSERT INTO public.transactions VALUES (4, 6700, 'primeiro dep teste2', 'deposit', '2021/06/24', 2);
-INSERT INTO public.transactions VALUES (5, 700, 'primeira retirada teste2', 'withdraw', '2021/06/24', 2);
-INSERT INTO public.transactions VALUES (6, 7000, 'testar o negativo', 'withdraw', '2021/06/24', 2);
-INSERT INTO public.transactions VALUES (7, 2000, 'testar o positivo', 'deposit', '2021/06/24', 2);
-INSERT INTO public.transactions VALUES (8, 100, 'teste de apagar', 'deposit', '2021/06/24', 2);
-INSERT INTO public.transactions VALUES (9, 100, 'carnval', 'withdraw', '2021/06/25', 2);
-INSERT INTO public.transactions VALUES (10, 700, 'carnaval 2', 'withdraw', '2021/06/25', 2);
-INSERT INTO public.transactions VALUES (11, 400, 'salario 2', 'deposit', '2021/06/25', 2);
-INSERT INTO public.transactions VALUES (12, 100, 'last test', 'deposit', '2021/06/25', 2);
-INSERT INTO public.transactions VALUES (13, 200, 'last teste saida', 'withdraw', '2021/06/25', 2);
-INSERT INTO public.transactions VALUES (14, 300, 'salario', 'deposit', '2021/08/10', 20);
-INSERT INTO public.transactions VALUES (15, 3000, 'colegio', 'withdraw', '2021/08/10', 20);
-INSERT INTO public.transactions VALUES (16, 300000, 'salário', 'deposit', '2021/08/10', 22);
-INSERT INTO public.transactions VALUES (17, 120000, 'despesa-colégio', 'withdraw', '2021/08/10', 22);
-INSERT INTO public.transactions VALUES (18, 300000, 'salário', 'deposit', '2021/08/10', 23);
-INSERT INTO public.transactions VALUES (19, 120000, 'despesa-colégio', 'withdraw', '2021/08/10', 23);
-INSERT INTO public.transactions VALUES (20, 3400, 'mesada', 'deposit', '2021/08/11', 24);
-INSERT INTO public.transactions VALUES (21, 1256, 'despesa-lanche', 'withdraw', '2021/08/11', 24);
+
 
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.users VALUES (1, 'Teste', 'teste@gmail.com', '$2b$12$7z2iZlRDUtGTgFpkPScFf.di4m01r2Ovyc7TCMfuyikW/AeR0ypaG');
-INSERT INTO public.users VALUES (2, 'jteste2', 'jteste2@gmail.com', '$2b$12$8m8Yu6H4WQVMqY.M0a6NEu9MMZc6IM5tWZ225ZYbJ9QhSK1FE/WBq');
-INSERT INTO public.users VALUES (3, 'Jean', 'jeancarlomo@hotmail.com', '$2b$12$4vCICd1DtKgja8GP9MJwhuKwgIhKr1cWlCCR8RzASYxRKe2zWsj/2');
-INSERT INTO public.users VALUES (19, 'jean', '12345@gmail.com', '$2b$12$KRaMcKSJ1f5oq4pfqaoNHe/5CDyRzPX.P8tt7oi1EQYRSGWpGlp8C');
+
+INSERT INTO public.users VALUES (3, 'Jean', 'jeancarlomo@hotmail.com', '$2b$12$6YM7J1S/hvcp8.HZoX.OvOvsyJyq6EV.9gg3uvQ1BIVUDgVXqdgOG');
+
 INSERT INTO public.users VALUES (20, 'Jean Carlo', 'jteste34@gmail.com', '$2b$12$EXHGWZB2vhQ6S6RUEZms9er1TTK0mBwEQuonyVFjjNZcNBtSC6ZM.');
-INSERT INTO public.users VALUES (21, 'test1', 'test@gmail.com', '$2b$12$5IBlISfnRlWbaZwO1ZroOelaKwsoh.0ruBFz7q350//PDjSwN7AuW');
-INSERT INTO public.users VALUES (22, 'Pedro Farias', 'pedrof@gmail.com', '$2b$12$MOwRK/nJi4WU7rkE5MOup.fj9jZmcGLcxeAiKASpACLv4iDEBZHcK');
-INSERT INTO public.users VALUES (23, 'Jorge Gabriel', 'jorge@gmail.com', '$2b$12$f3wl.2z1Mb6sY.i0E1DiV.YrpQeznSL/PCCC.MAWAJ2nrTttqpEku');
-INSERT INTO public.users VALUES (24, 'Jean ', 'jteste89@gmail.com', '$2b$12$Umk1WQ7zzeHp6uD5U5vtUeaqS.wTtWjrunMP6p1mo4rKVqE33PjtK');
+INSERT INTO public.users VALUES (22, 'Pedro F', 'pedrof@gmail.com', '$2b$12$6YM7J1S/hvcp8.HZoX.OvOvsyJyq6EV.9gg3uvQ1BIVUDgVXqdgOG');
+INSERT INTO public.users VALUES (23, 'Jorge G', 'jorge@gmail.com', '$2b$12$6YM7J1S/hvcp8.HZoX.OvOvsyJyq6EV.9gg3uvQ1BIVUDgVXqdgOG');
+
 
 
 --
